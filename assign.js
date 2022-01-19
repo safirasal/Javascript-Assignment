99499bb9ace4dde077626e7c3a224d8dd1176324
safirasal <safira_salsabila_29rpl@student.smktelkom-mlg.sch.id>
2022-01-19 07:20:29 +0700

    My Project

---
+++ assign.js
@@ -0,0 +1,31 @@
+console.log(`--------------------soal no 1-----------------------`);
+let panjang = 20.5
+let lebar = 30
+let luasTanah = 0
+let hargaTanah = 1500000
+let totalTanah = 0
+let ppn = 0
+
+
+luasTanah = panjang * lebar
+totalTanah = luasTanah * hargaTanah
+ppn = 15/100 * totalTanah
+
+totalTanah += ppn
+console.log(`Total bayar = ${totalTanah}`);
+
+console.log(`--------------------soal no 2-----------------------`);
+let totalBelanja = 0
+let Beras = 0 
+let Gula = 0
+let Telur = 0
+let Minyak Goreng = 0
+let barang = [
+    { nama: "Beras" , harga: 10000, jumlah: 5 },
+    { nama: "Gula" , harga: 14000, jumlah: 5 },
+    { nama: "Telur" , harga: 20000, jumlah: 2 },
+    { nama: "Minyak Goreng" , harga: 9000, jumlah: 10 }
+]
+
+let harga
+
